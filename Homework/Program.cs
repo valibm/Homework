38c4c0bc1b75029d171dd7019b15cc28fc05217c
valibm <valibm@code.edu.az>
2022-03-16 21:52:00 +0400

    Add project files.

---
+++ Homework/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 678;
+            int floor = 0;
+            int x = 0;
+            while (number != 0)
+            {
+                x = number % 10;
+                number -= x;
+                number /= 10;
+                floor++;
+            }
+            Console.WriteLine(floor);
+        }
+    }
+}
